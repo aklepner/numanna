7ae3f953cb353fe2a1319327377e216c98a5d209
Andrew Klepner <andy@brighteridea.com>
2017-03-22 14:22:06 -0400

    Zoho Uninstall

--- app/code/local/HN/Zohointegration/controllers/IndexController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class HN_Zohointegration_IndexController extends Mage_Core_Controller_Front_Action {    
-	public function IndexAction(){
-		echo "ZohoCRM Integration</br><pre>";
-		$model = Mage::getModel('zohointegration/sync_account')->sync(95,true);
-		echo "</pre>";
-	}
-}
